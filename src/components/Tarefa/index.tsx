aed0e54607ed47ac188721620a189291a42bb28c
Matheus Ferretti <matheus10.ferretti@gmail.com>
2023-06-27 09:59:24 -0300

    Commit de atualizacao

---
+++ src/components/Tarefa/index.tsx
@@ -0,0 +1,44 @@
+import * as S from './styles'
+import { useState } from 'react'
+import * as enums from '../../uteis/enums/Tarefa'
+
+type Props = {
+  titulo: string
+  prioridade: enums.Prioridade
+  status: enums.Status
+  descricao: string
+}
+
+const Tarefa = ({ descricao, prioridade, titulo, status }: Props) => {
+  const [estaEditando, setEstaEditando] = useState(false)
+
+  return (
+    <S.Card>
+      <S.Titulo>{titulo}</S.Titulo>
+      <S.Tag parametro="prioridade" prioridade={prioridade}>
+        {prioridade}
+      </S.Tag>
+      <S.Tag parametro="status" status={status}>
+        {status}
+      </S.Tag>
+      <S.Descricao value={descricao} />
+      <S.BarraAcoes>
+        {estaEditando ? (
+          <>
+            <S.BotaoSalvar>Salvar</S.BotaoSalvar>
+            <S.BotaoCancelar onClick={() => setEstaEditando(false)}>
+              Cancelar
+            </S.BotaoCancelar>
+          </>
+        ) : (
+          <>
+            <S.Botao onClick={() => setEstaEditando(true)}>Editar</S.Botao>
+            <S.BotaoCancelar>Remover</S.BotaoCancelar>
+          </>
+        )}
+      </S.BarraAcoes>
+    </S.Card>
+  )
+}
+
+export default Tarefa
